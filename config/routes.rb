014efcf3c9ac204fc4018784e4887f821ec6528f
jerikchan <jerikchan@gmail.com>
2023-05-06 03:54:15 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root "events#index"
+
+  resources :events
+  namespace :admin do
+    root "events#index"
+    resources :events
+  end
+end
